0cba1cc9ac4af230c85c3da8ceaca9306d850f2c
Yen-Lin Chen <3822365+hencrice@users.noreply.github.com>
2019-08-04 12:20:28 -0700

    Finished the `macro` exercises.

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,6 +5,16 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
+macro_rules! my_macro {
+    ($val:expr) => {
+        {
+            let mut owned_string: String = "Hello ".to_owned();
+            owned_string.push_str($val);
+            owned_string
+        }
+    };
+}
+
 fn main() {
     if my_macro!("world!") != "Hello world!" {
         panic!("Oh no! Wrong output!");
